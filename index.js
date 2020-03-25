f11d207e5ec3c9df0513354a9a4d0cde0c7ae115
Jack <jackrrye@gmail.com>
2020-03-25 13:32:37 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,17 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+// let obj = objArray.find(obj => obj.id == 3);
+
+function superbowlWin(arr) {
+  let thisWin = arr.find(function(win) {
+    return win.result === "W";
+  })
+  if(thisWin===undefined) {
+    return undefined;
+  }
+  else {
+    return thisWin.year
+  }
+}
