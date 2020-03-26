ae9be72e65ff6e2a06e730962e782abaa72abf59
Alec Hall <alecnhall@gmail.com>
2020-03-26 16:29:46 -0600

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(arrayOfObjects){
+  let win = arrayOfObjects.find(year => {
+    console.log(year.result === 'W')
+    return year.result == 'W'
+  });
+  if (win === undefined) {
+    return undefined;
+  }
+  return win.year;
+};
