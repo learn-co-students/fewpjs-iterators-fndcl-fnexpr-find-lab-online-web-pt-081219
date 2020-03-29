32dcc9ace1053c2a4952c21259666716328cfac5
kierra <51520037+kierrakay@users.noreply.github.com>
2020-03-28 20:20:07 -0400

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-const testVar = {}
-
-function testFunc() {
-  return "hi"
+superbowlWin = (record) => {
+  let result = record.find( record => record.result === "W")
+  return !!result ? result.year : undefined
 }
+
+
+
+
+
+// Write a function called superbowlWin() in index.js that:
+
+// Receives 1 argument, an Array of JavaScript Objects
+// Each object has two properties: year and result
+// Use find() to test each Object to see if the result is "W" — a win!
+// superbowlWin() should return the year when the win occurred (if it occurred at all!).
+//  If no win is found, it should return, sadly, undefined
