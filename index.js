a324c9181f45380bb1dee2191b5d414075e688be
Maria Le <hp.mars.le@gmail.com>
2020-04-06 14:15:16 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(record) {
+  let winner = record.find(winner => winner.result === "W")
+  if (winner) {
+    return winner.year
+  }
+}
