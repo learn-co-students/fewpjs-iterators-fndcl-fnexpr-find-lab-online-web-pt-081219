63217fee7a397168b5937e2502644854e0879cb5
helloimiskra <helloimiskra@gmail.com>
2020-04-13 20:19:29 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+
+function superbowlWin(record){
+  let winningYear = record.find(w => w.result === 'W')
+  if (!!winningYear){
+    return winningYear.year
+  }
+}
